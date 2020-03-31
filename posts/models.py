334f7ea263fa77de3f01e1cc4be2c13a7972ca55
adaniky <paladdick@gmail.com>
2020-03-31 13:26:42 +0300

    add post to model

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 # Create your models here.
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+class Post(models.Model):
+    title= models.CharField(max_length=300, unique=True)
+    url= models.SlugField(max_length=300)
+    content= models.TextField()
+    pub_date = models.DateTimeField(auto_now_add= True)
+    last_edited= models.DateTimeField(auto_now= True)
+    author= models.ForeignKey(User, on_delete=models.CASCADE)
+    likes= models.IntegerField(default=0)
+    dislikes= models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        self.url= slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+class Preference(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    post= models.ForeignKey(Post, on_delete=models.CASCADE)
+    value= models.IntegerField()
+    date= models.DateTimeField(auto_now= True)
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return str(self.user) + ':' + str(self.post) +':' + str(self.value)
+
+    class Meta:
+       unique_together = ("user", "post", "value")
