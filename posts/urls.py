c0b654aa7cb2ac0f03a3468981ba852a4b7a34d5
adaniky <paladdick@gmail.com>
2020-04-04 17:17:22 +0300

    add users api

--- posts/urls.py
+++ posts/urls.py
@@ -1,14 +1,22 @@
-from django.urls import path
-
+from django.urls import include, path
+from rest_framework import routers
 from . import views
 
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    # path('specifics/<int:question_id>/', views.detail, name='detail'),
-    # # ex: /polls/5/results/
-    # path('<int:question_id>/results/', views.results, name='results'),
-    # # ex: /polls/5/vote/
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+# urlpatterns = [
+#     # ex: /polls/
+#     path('', views.index, name='index'),
+#     # ex: /polls/5/
+#     # path('specifics/<int:question_id>/', views.detail, name='detail'),
+#     # # ex: /polls/5/results/
+#     # path('<int:question_id>/results/', views.results, name='results'),
+#     # # ex: /polls/5/vote/
+#     # path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
